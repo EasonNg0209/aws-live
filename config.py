fa00d9332e7d739e323da2db4a84228df1402117
EasonNg0209 <88933311+EasonNg0209@users.noreply.github.com>
2023-08-24 15:43:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.co30tbufbk4a.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "ngeason-employee"
+custombucket = "eason-employee"
 customregion = "us-east-1"
 
