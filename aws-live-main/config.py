01d30baf56c3329cecdce7023c15ebe6e03cdbdf
EasonNg0209 <88933311+EasonNg0209@users.noreply.github.com>
2023-08-24 15:32:01 +0800

    Delete aws-live-main directory

--- aws-live-main/config.py
+++
@@ -1,7 +0,0 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "ngeason-employee"
-customregion = "us-east-1"
-
